e0379a5b13f851c52dd48cae64a766957d666262
lsewilson <laurawilson@hotmail.co.uk>
2016-09-05 18:34:17 +0100

    adds sinatra application

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.3.1'
 
+gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'rspec'
--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -1,16 +1,14 @@
-
+require 'spec_helper'
 
 feature 'lists links' do
   scenario 'shows a list of links on homepage' do
     Link.create(url: 'https://www.facebook.com/home.php', title: 'Facebook')
-    Link.create(url: 'https://www.google.co.uk', title: 'Google')
-    visit '/home'
+    visit '/links'
 
     expect(page.status_code).to eq 200
 
-    within 'ul#home' do
+    within 'ul#links' do
       expect(page).to have_content('Facebook')
-      expect(page).to have_content('Google')
     end
   end
 end
