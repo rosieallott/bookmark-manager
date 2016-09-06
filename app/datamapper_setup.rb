a8919033aa88f78c5bb78e038aeebdd8a1f5cc61
matt vickers <matt.vickers@sky.com>
2016-09-06 16:45:24 +0100

    ch.15 implement tag filter view

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -4,6 +4,8 @@ require 'dm-postgres-adapter'
 require_relative 'models/link'
 require_relative 'models/tag'
 
-DataMapper.setup( :default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup( :default,"postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
+ # ENV['DATABASE_URL'] || 
