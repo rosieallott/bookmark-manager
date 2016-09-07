f73194b8079fe6d1a70873a0bad524e1d85ccf52
lsewilson <laurawilson@hotmail.co.uk>
2016-09-07 12:21:23 +0100

    re-adds heroku database url

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -4,8 +4,6 @@ require 'dm-postgres-adapter'
 require_relative 'models/link'
 require_relative 'models/tag'
 
-DataMapper.setup( :default,"postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup( :default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
-
- # ENV['DATABASE_URL'] || 
